cc481e4ab54041b597938dddf3be8bb2dc27ad11
Rabe1402 <huberrabe@gmail.com>
2025-06-02 19:44:51 +0200

    finnisched code. no jet tested

--- code/MacroKeyboard.ino
+++ code/MacroKeyboard.ino
@@ -23,6 +23,9 @@ const int ButtonPState[NButtons] = {}; // previous button states
 
 int Led1 = 7;  // Pin for the LED1
 
+unsigned long lastDebounceTime[NButtons] = {0};  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    //** the debounce time; increase if the output flickers
+
 const int statusVAR = 0; // Status variable, not used in this code but can be used for debugging or other purposes
 
 byte midiCh = 1; //* MIDI channel to be used
@@ -58,3 +61,158 @@ void loop()
 }
 
 // normal Buttons 
+void buttons() 
+{
+
+  for (int i = 0; i < NButtons; i++) 
+  {
+    buttonCState[i] = digitalRead(buttonPin[i]);  // read pins from arduino
+
+    if ((millis() - lastDebounceTime[i]) > debounceDelay) 
+    {
+      if (buttonPState[i] != buttonCState[i]) 
+      {
+        lastDebounceTime[i] = millis();
+        if (buttonCState[i] == LOW) 
+        {
+            //send node
+          noteOn(midiCh, note + i, 127);  // channel, note, velocity
+          MidiUSB.flush();
+        }
+        else 
+        {
+            //sends no note
+          noteOn(midiCh, note + i, 0);  // channel, note, velocity
+          MidiUSB.flush();
+
+        }
+        buttonPState[i] = buttonCState[i];
+      }
+    }
+  }
+}
+
+// debug Buttons
+void buttons_debug() 
+{
+  for (int i = 0; i < NButtons; i++) 
+  {
+    buttonCState[i] = digitalRead(buttonPin[i]);  // read pins from arduino
+
+    if ((millis() - lastDebounceTime[i]) > debounceDelay) 
+    {
+      if (buttonPState[i] != buttonCState[i]) 
+      {
+        lastDebounceTime[i] = millis();
+        if (buttonCState[i] == LOW) 
+        {
+          Serial.print("Button ");
+          Serial.print(i);
+          Serial.println(" pressed");
+          noteOn(midiCh, note + i, 127);  // channel, note, velocity
+          MidiUSB.flush();
+        }
+        else 
+        {
+          Serial.print("Button ");
+          Serial.print(i);
+          Serial.println(" released");
+          noteOn(midiCh, note + i, 0);  // channel, note, velocity
+          MidiUSB.flush();
+        }
+        buttonPState[i] = buttonCState[i];
+      }
+    }
+  }
+}
+
+// normal Pots
+void pots()
+{
+  for (int i = 0; i < NPots; i++) // Loops through all the potentiometers
+  { 
+    PotCState[i] = analogRead(pots[i]); // reads the pins from arduino
+
+    MidiCState[i] = map(PotCState[i], 0, 1023, 0, 127); // Maps the reading of the potCState to a value usable in midi
+
+    PotVar = abs(PotCState[i] - PotPState[i]); // Calculates the absolute value between the difference between the current and previous state of the pot
+
+    if (PotVar > VarThreshold) // Opens the gate if the potentiometer variation is greater than the threshold
+    { 
+      PTime[i] = millis(); // Stores the previous time
+    }
+
+    timer[i] = millis() - PTime[i]; // Resets the timer 11000 - 11000 = 0ms
+
+    if (timer[i] < TIMEOUT) // If the timer is less than the maximum allowed time it means that the potentiometer is still moving
+    { 
+      potMoving = true;
+    }
+    else 
+    {
+      potMoving = false;
+    }
+
+    if (potMoving == true) // If the potentiometer is still moving, send the change control
+    { 
+      if (midiPState[i] != midiCState[i]) 
+      {
+        // Sends  MIDI CC 
+        // Use if using with ATmega32U4 (micro, pro micro, leonardo...)
+        controlChange(midiCh, cc + i, midiCState[i]); //  (channel, CC number,  CC value)
+        MidiUSB.flush();
+
+        PotPState[i] = PotCState[i]; // Stores the current reading of the potentiometer to compare with the next
+        MidiPState[i] = MidiCState[i];
+      }
+    }
+  }
+}
+
+// debug Pots
+void pots_debug()
+{
+  for (int i = 0; i < NPots; i++) // Loops through all the potentiometers
+  { 
+    PotCState[i] = analogRead(pots[i]); // reads the pins from arduino
+
+    MidiCState[i] = map(PotCState[i], 0, 1023, 0, 127); // Maps the reading of the potCState to a value usable in midi
+
+    PotVar = abs(PotCState[i] - PotPState[i]); // Calculates the absolute value between the difference between the current and previous state of the pot
+
+    if (PotVar > VarThreshold) // Opens the gate if the potentiometer variation is greater than the threshold
+    { 
+      PTime[i] = millis(); // Stores the previous time
+    }
+
+    timer[i] = millis() - PTime[i]; // Resets the timer 11000 - 11000 = 0ms
+
+    if (timer[i] < TIMEOUT) // If the timer is less than the maximum allowed time it means that the potentiometer is still moving
+    { 
+      potMoving = true;
+      Serial.println("Pot movings");
+    }
+    else 
+    {
+      potMoving = false;
+    }
+
+    if (potMoving == true) // If the potentiometer is still moving, send the change control
+    { 
+      if (midiPState[i] != midiCState[i]) 
+      {
+        Serial.print("Potentiometer ");
+        Serial.print(i);
+        Serial.print(" changed to: ");
+        Serial.println(MidiCState[i]);
+        
+        // Sends MIDI CC 
+        controlChange(midiCh, cc + i, midiCState[i]); //  (channel, CC number,  CC value)
+        MidiUSB.flush();
+
+        PotPState[i] = PotCState[i]; // Stores the current reading of the potentiometer to compare with the next
+        MidiPState[i] = MidiCState[i];
+      }
+    }
+  }
+}
